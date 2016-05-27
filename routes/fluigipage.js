4bf864290fc5bf230d28b7bff28dd8a273fefbb3
priyakapadia <priyak@bu.edu>
2016-05-27 16:11:18 -0400

    Node Implementation with Fluigi Page

---
+++ routes/fluigipage.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('fluigipage', { title: 'Fluigi Page' });
+});
+
+module.exports = router;
+/**
+ * Created by Priya on 27/05/2016.
+ */

781b62c14becf6db7def7d6a6b1ea367b621aa41
Kestutis <kestas@bu.edu>
2016-05-27 16:44:07 -0400

    Shane cleaning up settings branch

--- FluigiGUI/app.js
+++ FluigiGUI/app.js
@@ -4,13 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var multer= require('multer');
 
-var fs = require('fs');
-
-var users = require('./routes/users');
 var routes = require('./routes/index');
-var fluigipage = require('./routes/fluigipage');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -28,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/fluigipage',fluigipage);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

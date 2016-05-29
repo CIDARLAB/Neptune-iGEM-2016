cfe1874ef46ae440aef59296c5dd8d556cee539c
Johan Ospina <Johanos@bu.edu>
2016-05-28 20:25:49 -0400

    got terminal interaction

--- FluigiGUI/app.js
+++ FluigiGUI/app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes              = require('./routes/index');
+var users               = require('./routes/users');
 
 var app = express();
 
--- FluigiGUI/routes/index.js
+++ FluigiGUI/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var serialcommunication = require('../serialcommunication');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+/* GET Serial Communication page. */
+router.get('/serialcommunication', function(req, res, next) {
+  //res.render('serialcommunication', { title: 'COM' });
+  res.send(serialcommunication.command());
+});
+
+
 
 module.exports = router;

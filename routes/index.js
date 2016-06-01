9bc20af0d905441709cdd5eb7d6ceb697b56d722
priyakapadia <priyak@bu.edu>
2016-06-01 11:54:44 -0400

    Updated Settings with Commented File Saving

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var multer= require('multer');
 var upload = multer({ storage : storage}).single('userPhoto');
-
+var fs = require('fs');
 
 
 /* GET home page. */
@@ -18,7 +18,7 @@ router.get('/fluigipage', function(req, res, next) {
 // Store Files
 var storage =   multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, './uploads');
+    callback(null, '/public/uploads');
   },
   filename: function (req, file, callback) {
     callback(null, file.fieldname + '-' + Date.now());
@@ -29,15 +29,28 @@ router.get('/fluigipage',function(req,res){
   res.sendFile(__dirname + "/public/uploads");
 });
 
-router.post('/api/photo',function(req,res){
+
+router.post('/api/photo', function(req,res){
   upload(req,res,function(err) {
     if(err) {
       return res.end("Error uploading file.");
     }
     res.end("File is uploaded");
+    console.log("My file: "+res);
+
+    // fs.writeFile("mysvg.svg", (res.body), function(err){
+    //   if(err) {
+    //     return console.log(err);
+    //   }
+    //   else {
+    //     console.log("The file was saved!");
+    //   }
+    // });
   });
 });
 
 
 module.exports = router;
+
+
 

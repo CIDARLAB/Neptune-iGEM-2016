9c264f69710fedec7edbd9212515f57128976600
Rebecca Wolf <rcwolf@bu.edu>
2016-06-10 12:01:45 -0400

    fixed SVG display with 3duf output and taking JSON, parse, and use stored values to populate graph with ports

--- public/javascripts/uploadBar.js
+++ public/javascripts/uploadBar.js
@@ -2,6 +2,13 @@
 
 //Upload AJAX Forms
 
+// Initialize localStorage values
+
+
+
+
+
+
 $(document).ready(function () {
 
     $('#uploadForm').submit(function () {
@@ -20,12 +27,15 @@ $(document).ready(function () {
                 // Json successfully uploaded
                 var fileOfChoice = "../uploads/myjson.json";
                 $.getJSON(fileOfChoice, function (json) {
-                    //alert('awesome possum');
-
 
                     // Use Json as a string
                     jsonString = JSON.stringify(json);
                     var controlOnly;
+
+                    // find size of entire SVG from JSON
+                    localStorage.SVGdimX = JSON.stringify(Object(json.params.width));
+                    localStorage.SVGdimY = JSON.stringify(Object(json.params.height));
+
 
                     // figure out indices of flow and control and cut json_string to substring to only contain control
                     flowIndex = jsonString.search('flow');
@@ -48,39 +58,21 @@ $(document).ready(function () {
 
 
                     while ((myArray = Re.exec(controlOnly)) !== null) {
-                        //console.log(myArray);
                         portX.push(myArray[1]);
                         portY.push(myArray[2]);
                         portRadius1.push(myArray[3]);
                         portRadius2.push(myArray[4]);
                         portArray.push(myArray.index);
                     }
-                    console.log("Port string index: " + portArray);
-                    console.log("Port X coordinates: " + portX);
-                    console.log("Port Y coordinates: " + portY);
-                    console.log("Port Radius1: " + portRadius1);
-                    console.log("Port Radius2: " + portRadius2);
-
-                    // Store json variables to localStorage
-                    localStorage.portX = portX;
-                    localStorage.portY = portY;
-                    localStorage.portRadius1 = portRadius1;
-                    localStorage.portRadius2 = portRadius2;
 
 
+                    // Store json variables to localStorage in form of JSON object...
+                    localStorage.portXcoords = JSON.stringify(portX);
+                    localStorage.portYcoords = JSON.stringify(portY);
+                    localStorage.portRadius1vals = JSON.stringify(portRadius1);
+                    localStorage.portRadius2vals = JSON.stringify(portRadius2);
 
-
-
-                    // method using Json as JSON OBJECT
-                    //    for( var i=0; i < (Object.keys(json.layers)).length; i++) { // there will be 2 -> flow and control
-                    //        //console.log(json.layers[i].name);
-                    //        //console.log(Object.keys(json.layers[i].params));
-                    //        if(json.layers[i].name == 'flow') {
-                    //            console.log(Object.keys(json.layers[0].features));
-                    //            //console.log(json.layers[i].name);
-                    //        }
-                    //    }
-
+                    //console.log("Port x coordinates from localStorage: " + JSON.parse(localStorage.getItem('portXcoords'))[1]);
 
                 });
 
@@ -100,6 +92,8 @@ $(document).ready(function () {
             success: function (response) {
                 console.log(response);
                 $("#status2").empty().text(response);
+                // refresh the page so that svg loads
+                location.reload();
 
             }
         });

2cba7b56617016bbbeb51f64dfe0412f2cea838d
kestaskk <kestas@bu.edu>
2016-06-13 15:47:28 -0400

    Working Serial Communication with Arduino
    This GUI is capable of talking to the Arduino :)

--- public/javascripts/fluigiGraph.js
+++ public/javascripts/fluigiGraph.js
@@ -1,31 +1,32 @@
 //Graphing the SVG
 
 function onclickanchortag(){
-    //console.log('hey beb');
+    console.log('hey beb');
     var location = getLocation(this.src);
     console.log( );
 
     switch (location.pathname){
         case "/images/fluigi/valveMarkerOpen.svg":
             $(this).attr("src", "../images/fluigi/valveMarkerClosed.svg" );
-            console.log(this.id);
-
+            var myid = $(this).id;
+            flipFlop_valveState(myid);
             break;
 
         case "/images/fluigi/valveMarkerClosed.svg":
             $(this).attr("src", "../images/fluigi/valveMarkerOpen.svg" );
-            console.log(this.id);
+            var myid = $(this).id;
+            flipFlop_valveState(myid);
             break;
 
         default:
             $(this).attr("src", "../images/fluigi/valveMarkerClosed.svg" );
+            //var myid = $(this).id;
+            //flipFlop_valveState(myid);
             break;
     }
     if (location.pathname == "/images/fluigi/valveMarkerOpen.svg"){
 
     }
-
-
 
 
     return false;
@@ -39,9 +40,6 @@ var getLocation = function(href) {
     l.href = href;
     return l;
 };
-
-
-
 
 
 // GRAPH FORMATTING
@@ -58,39 +56,39 @@ $(document).ready(function(){
 
 
     // load svg to canvas and reformat
-     var svgGraph = document.createElement("img");
+    var svgGraph = document.createElement("img");
 
-     svgGraph.src = "../uploads/mysvg.svg";
+    svgGraph.src = "../uploads/mysvg.svg";
 
-     svgGraph.addEventListener("load", function () {
+    svgGraph.addEventListener("load", function () {
 
 
-         // original width and height of SVG upon load
-         SVGwidth = JSON.parse(localStorage.getItem('SVGdimX'));
-         SVGheight = JSON.parse(localStorage.getItem('SVGdimY'));
-         // console.log('SVG width: ' + SVGwidth);
+        // original width and height of SVG upon load
+        SVGwidth = JSON.parse(localStorage.getItem('SVGdimX'));
+        SVGheight = JSON.parse(localStorage.getItem('SVGdimY'));
+        // console.log('SVG width: ' + SVGwidth);
 
 
 
         // ratio to preserve aspect ratio of svg
-         SVGscaleX = (canWidth) / SVGwidth;
-         SVGscaleY = (canHeight) / SVGheight;
+        SVGscaleX = (canWidth) / SVGwidth;
+        SVGscaleY = (canHeight) / SVGheight;
 
-         localStorage.setItem('SVGscaleX', SVGscaleX);
-         localStorage.setItem('SVGscaleY', SVGscaleY);
+        localStorage.setItem('SVGscaleX', SVGscaleX);
+        localStorage.setItem('SVGscaleY', SVGscaleY);
 
-         //console.log('SVG scale x: ' + JSON.parse(localStorage.getItem('SVGscaleX')));
+        //console.log('SVG scale x: ' + JSON.parse(localStorage.getItem('SVGscaleX')));
 
 
-         // cx.drawImage(svgGraph, SVGx - 200, -10 * SVGscale, SVGwidth * SVGscale, SVGheight * SVGscale);
-         cx.drawImage(svgGraph, 0, 0, canWidth, canHeight);
-     });
+        // cx.drawImage(svgGraph, SVGx - 200, -10 * SVGscale, SVGwidth * SVGscale, SVGheight * SVGscale);
+        cx.drawImage(svgGraph, 0, 0, canWidth, canHeight);
+    });
 
 
 
 
     // for each pump, create new instance of valve template
-    for(i=0; i<JSON.parse(localStorage.getItem('portXcoords')).length; i++){
+    for(var i=0; i<JSON.parse(localStorage.getItem('portXcoords')).length; i++){
 
         var content = $("#content");
         var template = document.getElementById("valve-template").content.cloneNode(true);
@@ -107,7 +105,7 @@ $(document).ready(function(){
 
         var specificImage = template.querySelector('.valve_color');
         // set id of each valve anchor based on location in array
-        specificImage.id = 'valve'+i;
+        specificImage.id = i;
 
         specificImage.onclick = onclickanchortag;
 
@@ -116,4 +114,5 @@ $(document).ready(function(){
 
     }
 
- });
+});
+

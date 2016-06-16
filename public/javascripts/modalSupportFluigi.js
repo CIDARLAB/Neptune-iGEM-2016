fc4059abfe79920dd73745f25bfeb8ee1c3dac4d
kestaskk <kestas@bu.edu>
2016-06-15 23:28:08 -0400

    Implemented "debug" page for controlling individual motors with specific PWM values.
    This is a settings page for actually controlling the motors. We can
    expand more here later, but for now you can select a  motor and apply a
    specific PWM value to it, which is helpful for calibrating the motors.

---
+++ public/javascripts/modalSupportFluigi.js
@@ -0,0 +1,15 @@
+/**
+ * Created by kestas on 6/15/2016.
+ */
+
+function openDebugger()
+{
+    (document.getElementById('debugger_modal')).style.display = "block";
+}
+
+function closeDebugger()
+{
+    (document.getElementById('debugger_modal')).style.display = "none";
+}
+
+

4093b38f77a1c51059158c408410a05f3fea94c2
kestaskk <kestas@bu.edu>
2016-06-17 16:14:46 -0400

    Serial history console
    I began working on the serial history console. No luck so far, we need
    to figure out how to append to a file. Maybe php? Or we need to figure
    out how to read from an html element into a string, from there we can
    recursively write the serial history back to the console.

--- public/javascripts/modalSupportFluigi.js
+++ public/javascripts/modalSupportFluigi.js
@@ -12,4 +12,17 @@ function closeDebugger()
     (document.getElementById('debugger_modal')).style.display = "none";
 }
 
+function openSerialConsole()
+{
+    (document.getElementById('serialConsole_modal')).style.display = "block";
+}
 
+function closeSerialConsole()
+{
+    (document.getElementById('serialConsole_modal')).style.display = "none";
+}
+
+function writeToSerialConsole(serial_data)
+{
+
+}

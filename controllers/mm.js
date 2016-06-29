2b05554fef146c88933f2d6441193d78049e7639
aonanam <aonanam@bu.edu>
2016-06-29 12:27:19 -0400

    Initial Commit for MM Integration Branch

--- controllers/mm.js
+++ controllers/mm.js
@@ -2,6 +2,20 @@
  * Created by Priya on 22/06/2016.
  */
 
-exports.openMMPage= function(req, res){
-    res.render('uShroomPage', {title: 'MM Page'});
+exports.openMMPage = function (req, res) {
+  res.render('uShroomPage', {title: 'MM Page'});
+};
+var exec = require('child_process').exec, child;
+
+exports.executeJAR = function (req, res) {
+  child = exec('/usr/bin/java -jar ~/Applications/example.jar',
+    function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+      //save files that MM/Fluigi output (possibly with res)
+    });
+
 };

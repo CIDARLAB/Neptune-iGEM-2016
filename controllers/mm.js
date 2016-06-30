b44abe6d89ba4c085109205f289d5225445d52e8
aonanam <aonanam@bu.edu>
2016-06-30 16:59:49 -0400

    Backend .jar call to run MM implemented

--- controllers/mm.js
+++ controllers/mm.js
@@ -2,20 +2,44 @@
  * Created by Priya on 22/06/2016.
  */
 
-exports.openMMPage = function (req, res) {
+exports.openMMPage = function (req, res)
+{
   res.render('uShroomPage', {title: 'MM Page'});
 };
-var exec = require('child_process').exec, child;
 
-exports.executeJAR = function (req, res) {
-  child = exec('/usr/bin/java -jar ~/Applications/example.jar',
-    function (error, stdout, stderr) {
-      console.log('stdout: ' + stdout);
-      console.log('stderr: ' + stderr);
-      if (error !== null) {
-        console.log('exec error: ' + error);
-      }
-      //save files that MM/Fluigi output (possibly with res)
-    });
+const spawn = require('child_process').spawn;
+const mm = spawn('java', ['-jar', 'MuShroomMapper.jar', '-l ' + lfrFile, '-u ' + ucfFile]);
 
-};
+mm.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+mm.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+mm.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+/*
+ var mmJAR = require('child_process').spawn('java', ['-jar', 'MuShroomMapper.jar', '-l ' + lfrFile, '-u ' + ucfFile]);
+
+
+
+ var exec = require('child_process').exec, child;
+
+ exports.executeJAR = function (req, res) {
+ child = exec('java' ['-jar', 'MuShroomMapper.jar', '']);
+ function (error, stdout, stderr) {
+ console.log('stdout: ' + stdout);
+ console.log('stderr: ' + stderr);
+ if (error !== null)
+ {
+ console.log('exec error: ' + error);
+ }
+ //save files that MM/Fluigi output (possibly with res)
+ });
+
+ };
+ */

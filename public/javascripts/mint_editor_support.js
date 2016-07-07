9298019021a4f2b3d17fbf24dd5de0df9318de88
kestaskk <kestas@bu.edu>
2016-07-07 10:05:48 -0400

    Working file upload, edit, and save system!
    As of this commit, users can upload a MINT file, edit the MINT file, and
    save their changes. They CANNOT yet download the newly edited MINT file,
    but this feature will be soon to come.
    
    Some notes:
    - Ability to change theme, font, font size, syntax rules, is in dev
    - Editor needs styling. Currently, the editor does not happily support
    resizing or other window changes.
    - Do we want a master editor?
    
    To do:
    - "tray" for file organization,
    - LFR editor page
    - String the entire file system together to a single coherent workflow

--- public/javascripts/mint_editor_support.js
+++ public/javascripts/mint_editor_support.js
@@ -5,11 +5,12 @@ if (localStorage.firstVisitUshroom == true || localStorage.firstVisitUshroom == 
 {}
 else
     {
-        for (var i = 0; i < localStorage.MintEditorFileSize; i++)
-        {
-            editor.session.replace({
-                start: {row: i, column: 0},
-                end: {row: i, column: Number.MAX_VALUE}
-            }, JSON.parse(localStorage.MINT_EDITOR_SESSION)[i])
-        }
+        // editor.session.setValue('');
+        // for (var i = 0; i < localStorage.MintEditorFileSize; i++)
+        // {
+        //     editor.session.replace({
+        //         start: {row: i, column: 0},
+        //         end: {row: i, column: Number.MAX_VALUE}
+        //     }, JSON.parse(localStorage.MINT_EDITOR_SESSION)[i])
+        // }
     }

c29bb3c52e7bf22fad165605a972ce2a6c34406a
kestaskk <kestas@bu.edu>
2016-07-08 10:47:54 -0400

    File Upload System Refactored
    New and happily improved file system is working! Changes include:
    - Instead of one "uploads" folder, we now segregate our uploads based on
    the stage of the workflow the user is in,
    --> uploads/Specify
    --> uploads/Design
    --> uploads/Build_Verify
    
    This will make a organized file "tray" easier to implement.

--- public/javascripts/initiate_data_editor.js
+++ public/javascripts/initiate_data_editor.js
@@ -14,13 +14,4 @@ if (localStorage.firstVisitUshroom == true || localStorage.firstVisitUshroom == 
     localStorage.firstVisitUshroom = false;
 }
 else
-{
-    // editor.session.setValue('');
-    // for (var i = 0; i < localStorage.MintEditorFileSize; i++)
-    // {
-    //     editor.session.replace({
-    //         start: {row: i, column: 0},
-    //         end: {row: i, column: Number.MAX_VALUE}
-    //     }, JSON.parse(localStorage.MINT_EDITOR_SESSION)[i])
-    // }
-}
+{}

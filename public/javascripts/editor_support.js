5d0519af29ecb47f8bb8e8254444970cc9a95d05
kestaskk <kestas@bu.edu>
2016-07-14 18:31:59 -0400

    Bootstrap integration for SPECIFY and DESIGN pages.
    Whoo! Bootstrap has been successfully added! We not have:
    - Dashboard with a workflow overview
    - Specify page: LFR, UCF Editor; LFR, UCF uploader
    - Design page: MINT Editorl MINT, INI uploader
    - File tray with overview of workflow files

--- public/javascripts/editor_support.js
+++ public/javascripts/editor_support.js
@@ -2,6 +2,103 @@
  * Created by kestas on 7/8/2016.
  */
 
+function changeSpecifyTabs(Editor,Tab_To_Switch_To,SessionArray)
+{
+    switch (Tab_To_Switch_To)
+    {
+        case 'LFRtab':
+            document.getElementById('LFRtab').className = 'active';
+            document.getElementById('UCFtab').className = '';
+            document.getElementById('INItab').className = '';
+            localStorage.specify_editor_state = 'specifyLFR';
+            Editor.setSession(SessionArray[0]);
+            break;
+        case 'UCFtab':
+            document.getElementById('LFRtab').className = '';
+            document.getElementById('UCFtab').className = 'active';
+            document.getElementById('INItab').className = '';
+            localStorage.specify_editor_state = 'specifyUCF';
+            Editor.setSession(SessionArray[1]);
+            break;
+        case 'INItab':
+            document.getElementById('LFRtab').className = '';
+            document.getElementById('UCFtab').className = '';
+            document.getElementById('INItab').className = 'active';
+            localStorage.specify_editor_state = 'specifyINI';
+            Editor.setSession(SessionArray[2]);
+            break;
+    }
+
+
+}
+function saveEditorContent(Editor_To_Save_Content,FILE_TYPE)
+{
+    var EDITOR_SESSION = [];
+    var file_size = Editor_To_Save_Content.session.getLength();
+    for (var i = 0; i < file_size; i++)
+    {
+        var tempLine = Editor_To_Save_Content.session.getLine(i);
+        EDITOR_SESSION.push(tempLine);
+    }
+    switch(FILE_TYPE)
+    {
+        case 'specifyLFR':
+            localStorage.FILE_specifyLFR =  JSON.stringify(EDITOR_SESSION);
+            break;
+        case 'specifyUCF':
+            localStorage.FILE_specifyUCF =  JSON.stringify(EDITOR_SESSION);
+            break;
+        case 'specifyINI':
+            localStorage.FILE_specifyINI =  JSON.stringify(EDITOR_SESSION);
+            break;
+        case 'LFR':// Fix
+            localStorage.LFR_STRING =  JSON.stringify(EDITOR_SESSION);
+            break;
+        case 'MINT':// Fix
+            localStorage.MINT_STRING =  JSON.stringify(EDITOR_SESSION);
+            break;
+        case 'UCF':// Fix
+            localStorage.UCF_STRING =  JSON.stringify(EDITOR_SESSION);
+            break;
+    }
+}
+
+function downloadFile(File_Name,FILE_TYPE)
+{
+    var string_to_write = '';
+    var file_name = File_Name; // NOTE: File_Name argument is overwritten internally, for now
+    switch(FILE_TYPE)
+    {
+        case 'specifyLFR':
+            string_to_write = localStorage.FILE_specifyLFR;
+            file_name = 'specifyLFR';
+            break;
+        case 'specifyUCF':
+            string_to_write = localStorage.FILE_specifyUCF;
+            file_name = 'specifyUCF';
+            break;
+        case 'specifyINI':
+            string_to_write = localStorage.FILE_specifyINI;
+            file_name = 'specifyINI';
+            break;
+        case 'UCF File (Design)':
+            string_to_write = localStorage.UCF_STRING;
+            break;
+        case 'MINT File (Design)':
+            string_to_write = localStorage.MINT_STRING;
+            break;
+        case 'JSON File (Build)':
+            break;
+        case 'SVG File (Build)':
+            break;
+    }
+
+    $.post("/api/writeToFile",{fileData: string_to_write});
+    //window.open('../public/downloads/test.txt');
+    uriContent = "data:application/octet-stream," + encodeURIComponent(string_to_write);
+    newWindow = window.open(uriContent, 'New Document');
+}
+
 function fill_editor(File_To_Fill_Editor_With,Editor_To_Fill)
 {
     // Clear Editor before adding our file 
@@ -25,24 +122,24 @@ function pushFileToEditor(Editor_To_Push_Toward,FILE_TYPE)
     var CONTENT_TO_PUSH = [];
     switch(FILE_TYPE)
     {
-        case 'LFR_start':
-            $.get('../uploads/Specify/myLFR_start.txt',function(data)
+        case 'specifyLFR':
+            $.get('../uploads/Specify/specifyLFR.txt',function(data)
             {
                 CONTENT_TO_PUSH = data.split("\n");
                 fill_editor(CONTENT_TO_PUSH,Editor_To_Push_Toward);
             });
             //CONTENT_TO_PUSH = JSON.parse(localStorage.LFR_start_STRING);
             break;
-        case 'LFR':
-            $.get('../uploads/Design/myLFR.txt',function(data)
+        case 'specifyUCF':
+            $.get('../uploads/Design/specifyUCF.txt',function(data)
             {
                 CONTENT_TO_PUSH = data.split("\n");
                 fill_editor(CONTENT_TO_PUSH,Editor_To_Push_Toward);
             });
             //CONTENT_TO_PUSH = JSON.parse(localStorage.LFR_STRING);
             break;
-        case 'MINT':
-            $.get('../uploads/Design/myMINT.txt',function(data)
+        case 'specifyINI':
+            $.get('../uploads/Design/specifyINI.txt',function(data)
             {
                 CONTENT_TO_PUSH = data.split("\n");
                 fill_editor(CONTENT_TO_PUSH,Editor_To_Push_Toward);
@@ -65,33 +162,6 @@ function refreshEditor(File_To_Fill_Editor_With, Editor_To_Refresh)
     fill_editor(File_To_Fill_Editor_With, Editor_To_Refresh);
 }
 
-function saveEditorContent(Editor_To_Save_Content,FILE_TYPE)
-{
-    var EDITOR_SESSION = [];
-    var file_size = Editor_To_Save_Content.session.getLength();
-    for (var i = 0; i < file_size; i++)
-    {
-        var tempLine = Editor_To_Save_Content.session.getLine(i);
-        EDITOR_SESSION.push(tempLine);
-    }
-    switch(FILE_TYPE)
-    {
-        case 'LFR_start':
-            localStorage.LFR_start_STRING =  JSON.stringify(EDITOR_SESSION);
-            break;
-        case 'LFR':
-            localStorage.LFR_STRING =  JSON.stringify(EDITOR_SESSION);
-            break;
-        case 'MINT':
-            localStorage.MINT_STRING =  JSON.stringify(EDITOR_SESSION);
-            break;
-        case 'UCF':
-            localStorage.UCF_STRING =  JSON.stringify(EDITOR_SESSION);
-            break;
-    }
-
-}
-
 function saveEditorOptions(Editor_To_Update,theme_ID,syntax_ID,fontSize_ID,fontFamily_ID)
 {
     var THEME =  (document.getElementById(theme_ID)).value;
@@ -112,37 +182,7 @@ function saveEditorOptions(Editor_To_Update,theme_ID,syntax_ID,fontSize_ID,fontF
     });
 }
 
-function downloadFile(File_Type,File_Name)
-{
-    var string_to_write = '';
-    var file_name = File_Name;
-    switch(File_Type)
-    {
-        case 'LFR File (Specify)':
-            string_to_write = localStorage.LFR_start_STRING;
-            break;
-        case 'UCF File (Specify)':
-            break;
-        case 'LFR File (Design)':
-            string_to_write = localStorage.LFR_STRING;
-            break;
-        case 'UCF File (Design)':
-            string_to_write = localStorage.UCF_STRING;
-            break;
-        case 'MINT File (Design)':
-            string_to_write = localStorage.MINT_STRING;
-            break;
-        case 'JSON File (Build)':
-            break;
-        case 'SVG File (Build)':
-            break;
-    }
 
-    $.post("/api/writeToFile",{fileData: string_to_write});
-    //window.open('../public/downloads/test.txt');
-    uriContent = "data:application/octet-stream," + encodeURIComponent(string_to_write);
-    newWindow = window.open(uriContent, 'New Document');
-}
 
 function openDownloadModal()
 {

1044522af652cedd44e12bfd59418a8cc25c32ed
Rebecca Wolf <rcwolf@bu.edu>
2016-07-29 17:01:11 -0400

    in the process of setting up dispense commands with arduino

--- public/javascripts/fluigipage.js
+++ public/javascripts/fluigipage.js
@@ -215,6 +215,65 @@ function setNumberOfDispensers_JSON() {
 }
 
 
+function increaseDispenserOutput(dispenser_to_control)
+{
+    localStorage.dispenserToControl = dispenser_to_control;
+    // assumes the capacity of the syringe is 9 mL
+    if (JSON.parse(localStorage.dispenserData)[dispenser_to_control - 1]['Current_State'] <= JSON.parse(localStorage.dispenserData)[dispenser_to_control - 1]['Max']) {
+        var temp = JSON.parse(localStorage.dispenserData);//[valve_to_control]['Physical_State'] = 1;
+        temp[dispenser_to_control - 1]['Current_State'] += 1;
+        localStorage.dispenserData = JSON.stringify(temp);
+        sendCommandDispense();
+    }
+    else {
+        toastr.info('You have already dispensed the full amount of this syringe.');
+    }
+    return false;
+}
+
+function wrap_data_for_Arduino_Dispense()
+{
+    var dispenser_to_control = localStorage.dispenserToControl;
+    
+    // FIRST, PAD THE VALVE_TO_CONTROL WITH 0's SUCH THAT THE VALUE IS 3 CHARACTERS LONG
+    var dispenser_to_control_padded = zeroFill(dispenser_to_control,4);
+    // SECOND, PAD THE ML VALUE WITH 0's SUCH THAT THE VALUE IS 4 CHARACTERS LONG
+    var MLval_padded = zeroFill(JSON.parse(localStorage.dispenserData)[dispenser_to_control - 1]['Current_State'],4);
+    // CONCAT THE VALVE NUMBER AND PWM VALUE
+    var pre_command = dispenser_to_control_padded.concat(MLval_padded);
+    // ADD A START CODON TO SIGNIFY THE BEGINING OF SIGNAL
+    var startStr = '';
+    var pre_command_s = startStr.concat(pre_command);
+    // ADD A STOP CODON TO SIGNIFY THE END OF SIGNAL
+    var command = pre_command_s.concat('\n');
+    // RETURN THE DATA
+    return command;
+}
+
+function sendCommandDispense()
+{
+    var command = wrap_data_for_Arduino_Dispense();
+    var stringgggg = "Sending to Arduino: ";
+    var command_info = stringgggg.concat(command);
+    // --- Include code to serial.write() the command to the Arduino here --- //
+    toastr.info(command_info);
+    // writeToSerialConsole(command_info);
+    console.log(command);
+    localStorage.setItem('myCommand', command);
+    // document.forms.form1.area.value = document.forms.form1.area.value + '\nSerial Command Sent: ' + localStorage.myCommand;
+
+    $.ajax(
+        {
+            url: "/serialcommunication/send", type: 'POST', async: true,
+            data: {
+                commandData: command
+            },
+            success: function (response) {
+            },
+            error: function (response) {
+            }
+        });
+}
 
 
 
@@ -274,9 +333,6 @@ function sendCommand()
     localStorage.setItem('myCommand', command);
     // document.forms.form1.area.value = document.forms.form1.area.value + '\nSerial Command Sent: ' + localStorage.myCommand;
 
-
-
-
     $.ajax(
         {
             url: "/serialcommunication/send", type: 'POST', async: true,
@@ -288,10 +344,6 @@ function sendCommand()
             error: function (response) {
             }
         });
-
-
-
-
 }
 
 function inititateValveStates()

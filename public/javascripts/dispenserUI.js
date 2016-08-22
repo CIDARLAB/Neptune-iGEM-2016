56b286ce923fceb2fefd4f8427fe555575c3bdc7
Rebecca Wolf <rcwolf@bu.edu>
2016-08-22 13:27:08 -0400

    adding a new template for dispenser popup and setting up code for data binding to be later implemented

---
+++ public/javascripts/dispenserUI.js
@@ -0,0 +1,31 @@
+/**
+ * Created by rebeccawolf on 8/22/16.
+ */
+
+
+// DATA BINDING
+
+// function dispenserConstructor(element, data) {
+//     this.data = data;
+//     this.element = element;
+//     element.value = data;
+//     element.addEventListener("change", this, false);
+// }
+//
+//
+// dispenserConstructor.prototype.handleEvent = function(event) {
+//     switch (event.type) {
+//         case "change": this.change(this.element.value);
+//     }
+// };
+//
+// dispenserConstructor.prototype.change = function(value) {
+//     this.data = value;
+//     this.element.value = value;
+// };
+//
+//
+// var obj = new MyCtor(document.getElementById("foo"), "initial");
+// obj.change(obj.element.value + ++i);
+
+

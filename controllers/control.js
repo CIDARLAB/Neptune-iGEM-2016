95634f713a8e5a9f092091b8df998308f734c69f
Rebecca Wolf <rcwolf@bu.edu>
2016-08-25 11:53:58 -0400

    modal ready to be connected to backend calculations

--- controllers/control.js
+++ controllers/control.js
@@ -2,12 +2,11 @@
  * Created by Priya on 28/07/2016.
  */
 
-exports.openControllersPage = function(req, res)
-{
-    res.render('control', {title: 'Control'});
-};
-
 exports.openControlFullPage = function(req, res)
 {
     res.render('controlFull', {title: 'ControlFull'});
+};
+
+exports.runDispense = function(req, res) {
+    
 };

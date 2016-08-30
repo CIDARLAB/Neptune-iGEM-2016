63ac350ff33c0b781eafcdb1d5ccf2f042b41d2e
kestaskk <kestas@bu.edu>
2016-08-30 16:40:57 -0400

    IDE Implementatin
    Neptune is now an IDE

--- controllers/compileMint.js
+++ controllers/compileMint.js
@@ -7,6 +7,8 @@ var multer = require('multer');
 var express = require('express');
 var fs = require('fs');
 const readline = require('readline');
+const path = require('path');
+var replace = require("replace");
 
 io = require('socket.io')(global.server);
 io.on('connection', function(socket){
@@ -16,10 +18,37 @@ io.on('connection', function(socket){
 exports.compileMint = function(req, res)
 {
     var count = 0;
-    var file_path = './public/uploads/Design/designMINT.uf'; //req.body.filePath;
+
+    var mint_path  = req.body.filePathMINT;
+    var ini_path   = req.body.filePathINI;
+    var out_folder = req.body.outPath;
+    var name       = req.body.name;
+    var basename   = req.body.base_name;
+    var outputPath = path.join(out_folder,name);
+    var outp = outputPath;
+    outputPath = 'outputDirectory=' + outputPath;
+    var uName = '3D DEVICE ' + basename;
+
+    var svgloc = outp + basename + '_device_flow.svg';
+
+    replace({
+        regex: "outputDirectory=output/",
+        replacement: outputPath,
+        paths: [ini_path],
+        recursive: true,
+        silent: true,
+    });
+    replace({
+        regex: "3D DEVICE m1",
+        replacement: uName,
+        paths: [mint_path],
+        recursive: true,
+        silent: true,
+    });
+
 
     var par_terminal = require('child_process').spawn(
-        'java', ['-jar', './backend/Fluigi-jar-with-dependencies.jar', file_path, '-i', './public/uploads/Design/designINI.txt', '-o', 'sej']
+        'java', ['-jar', './backend/Fluigi-jar-with-dependencies.jar', mint_path, '-i', ini_path, '-o', 'sej']
     );
 
     par_terminal.stdout.on('data', function(data) {
@@ -34,6 +63,11 @@ exports.compileMint = function(req, res)
     par_terminal.on('close', (code) => {
         console.log(`child process exited with code ${code}`);
         if (code == 0) {
+
+            var read = fs.createReadStream(svgloc);
+            var write = fs.createWriteStream('./public/mysvg.svg');
+            read.pipe(write);
+
             res.send({terminalStatus: 'Success'});
             res.end();
         }

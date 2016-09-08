508e9dd4dd258c3dd8c4d2d36e534b3dd4050c7f
Rebecca Wolf <rcwolf@bu.edu>
2016-09-08 08:33:41 -0400

    minor changes to HW init scripts, functionality with user input in dispenser UIs

--- public/javascripts/dispenserUI.js
+++ public/javascripts/dispenserUI.js
@@ -2,23 +2,35 @@
  * Created by rebeccawolf on 8/22/16.
  */
 
-$(document).ready(function(){
-
-
-
-});
-
-
 function sendDispense(sender){
     // console.log("hello from " + sender.id);
     var form = sender.parentNode.parentNode;
     var volume = form.querySelector(".dispenseVol");
     var time = form.querySelector(".dispenseTime");
+    // console.log(volume.value);
+    // console.log(time.value);
 
-    console.log(volume.value);
-    console.log(time.value);
+    var dispenserID = sender.id;
+    dispenserID = dispenserID.replace(/\D/g,'');
+    console.log(dispenserID);
 
+    var currentVolume = JSON.parse(localStorage.dispenserData)[dispenserID - 1]['Current_State'];
 
+    var create_table_outputs = create_uL_table(32.6);
+    var PWM_table = create_table_outputs.PWM_table;
+    var uL_table = create_table_outputs.uL_table;
+    even_uL_steps(1, uL_table, PWM_table, 32.6, currentVolume, volume.value, time.value);
+    
+    return false;
+}
 
+function changeDispenseOrientation(sender) {
+    currentOrientation = sender.innerHTML;
+    if (currentOrientation === "pull") {
+        sender.innerHTML = "push";
+    }
+    else {      // currentOrientation === "push"
+        sender.innerHTML = "pull";
+    }
     return false;
 }

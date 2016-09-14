17a0c8709ecdd0bf3293cee90c9235b37e013fdb
kestaskk <kestas@bu.edu>
2016-09-14 09:41:06 -0400

    Implemented support for multiple output folders in a single project
    We can now have multiple output folders in a single project. Had to fix
    up and normalize file paths to get this to work.
    
    To make this more robust, I need to start adding configuration files or
    something similar to each output folder. This will make it easier to
    parse, and easier to interface with on the build and control pages

--- controllers/compileMint.js
+++ controllers/compileMint.js
@@ -8,7 +8,9 @@ var express = require('express');
 var fs = require('fs');
 const readline = require('readline');
 const path = require('path');
+var slash = require('slash');
 var replace = require("replace");
+var lineder = require( "lineder" );
 
 io = require('socket.io')(global.server);
 io.on('connection', function(socket){
@@ -21,6 +23,9 @@ exports.compileMint = function(req, res)
 
     var mint_path  = req.body.filePathMINT;
     var ini_path   = req.body.filePathINI;
+    var lfr_path = req.body.filePathLFR;
+    var ucf_path = req.body.filePathUCF;
+
     var out_folder = req.body.outPath;
     var name       = req.body.name;
     var basename   = req.body.base_name;
@@ -29,23 +34,42 @@ exports.compileMint = function(req, res)
     outputPath = 'outputDirectory=' + outputPath;
     var uName = '3D DEVICE ' + basename;
 
+    var configFileLoc = path.join(outp,'proj.ini');
+
     var svgloc = outp + basename + '_device_flow.svg';
 
-    replace({
-        regex: "outputDirectory=output/",
-        replacement: outputPath,
-        paths: [ini_path],
-        recursive: true,
-        silent: true,
-    });
-    replace({
-        regex: "3D DEVICE m1",
-        replacement: uName,
-        paths: [mint_path],
-        recursive: true,
-        silent: true,
+    var iniREGEX = '';
+    var mintREGEX = '';
+
+    lineder(ini_path).find("outputDirectory=",function(err,results){
+        //console.log(results);
+        iniREGEX = (results[0]).value;
+        iniREGEX = iniREGEX.substring(0, iniREGEX.length - 1);
+        iniREGEX = slash(iniREGEX);
+
+        //TODO: ERROR OCCURING; REPLACE PATH BREAKS CODE. FIX THIS
+        replace({
+            regex: iniREGEX,
+            replacement: slash(outputPath),
+            paths: [ini_path],
+            recursive: true,
+            silent: true,
+        });
     });
 
+    lineder(mint_path).find("3D DEVICE",function(err,results){
+        //console.log(results);
+        mintREGEX = (results[0]).value;
+        mintREGEX = mintREGEX.substring(0, mintREGEX.length - 1);
+        mintREGEX = slash(mintREGEX);
+        replace({
+            regex: mintREGEX,
+            replacement: slash(uName),
+            paths: [mint_path],
+            recursive: true,
+            silent: true,
+        });
+    });
 
     var par_terminal = require('child_process').spawn(
         'java', ['-jar', './backend/Fluigi-jar-with-dependencies.jar', mint_path, '-i', ini_path, '-o', 'sej']
@@ -67,6 +91,8 @@ exports.compileMint = function(req, res)
             var read = fs.createReadStream(svgloc);
             var write = fs.createWriteStream('./public/mysvg.svg');
             read.pipe(write);
+
+            var configFile = fs.openSync(configFileLoc,'w');
 
             res.send({terminalStatus: 'Success'});
             res.end();

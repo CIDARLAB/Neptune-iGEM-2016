aae34431fd0985f232e2d32c3577542250c969f6
Rebecca Wolf <rcwolf@bu.edu>
2016-09-14 13:02:10 -0400

    dispenser rate with up/down arrow functionality fixed

--- public/javascripts/dispenserUI.js
+++ public/javascripts/dispenserUI.js
@@ -3,36 +3,37 @@
  */
 
 function sendDispense(sender){
+
+    // VALUES FROM THE FORM:
     var form = sender.parentNode.parentNode;
     var volume = form.querySelector(".dispenseVol");
     var time = form.querySelector(".dispenseTime");
+    var dispenserID = sender.id;    // ID of dispenser you are controlling
+    dispenserID = dispenserID.replace(/\D/g,'');
     // console.log(volume.value);
     // console.log(time.value);
 
-    var dispenserID = sender.id;
-    dispenserID = dispenserID.replace(/\D/g,'');
+    var currentVolume = parseFloat(JSON.parse(localStorage.dispenserData)[dispenserID - 1]['Current_State']);   // current volume state of selected syringe
+    var precision = parseFloat(JSON.parse(localStorage.dispenserData)[dispenserID - 1]['Precision']);   // pull precision from settings table
 
-    var currentVolume = parseFloat(JSON.parse(localStorage.dispenserData)[dispenserID - 1]['Current_State']);
-    
     // Hardware computations
     var create_table_outputs = create_uL_table(32.6);
     var PWM_table = create_table_outputs.PWM_table;
     var uL_table = create_table_outputs.uL_table;
-    
-    // store conversion tables to be accessed by other parts of dispense operations
-    var storedConversions = JSON.parse(localStorage.dispenserConversions);
     var conversions = {};
     conversions = create_table_outputs.conversion_table;
-    storedConversions[dispenserID] = JSON.stringify(conversions);
-    localStorage.dispenserConversions = JSON.stringify(storedConversions);
+    console.log('conversions: ');
+    console.log(conversions);
     
+    // store conversion tables to be accessed by other parts of dispense operations
+    var storedConversions = JSON.parse(localStorage.dispenserConversions);  // load here so as not to overwrite tables already stored
+    storedConversions[dispenserID] = JSON.stringify(conversions);   // update computed conversions in temp variable
+    localStorage.dispenserConversions = JSON.stringify(storedConversions);  // store temp in localStorage
 
-    // update conversion chart for dispenser
-    JSON.parse(localStorage.dispenserData)[dispenserID - 1]['conversion'] = conversions;
+    var dispOrientation = JSON.parse(localStorage.dispenserData)[dispenserID - 1]['orientation'];   // determine whether is pull/push dispenser
+    var valueToDispense;
 
-    var dispOrientation = JSON.parse(localStorage.dispenserData)[dispenserID - 1]['orientation'];
-    var valueToDispense = 0;
-
+    // COMPUTE VALUE TO PASS INTO even_uL_steps()
     if(dispOrientation === "push"){
         valueToDispense = currentVolume - parseFloat(volume.value);
     }
@@ -40,56 +41,34 @@ function sendDispense(sender){
         valueToDispense = currentVolume + parseFloat(volume.value);
     }
 
-    // Will be defined when servo is selected in build/assembly
-    var precision = parseFloat(JSON.parse(localStorage.dispenserData)[dispenserID - 1]['Precision']);
-    PWMsteps = even_uL_steps(uL_table, PWM_table, precision, currentVolume, valueToDispense, time.value);   // [0] is seconds/step [1] is PWM value array to be sent
-
+    var even_uL_steps_output = even_uL_steps(uL_table, PWM_table, precision, currentVolume, valueToDispense, time.value);   // [0] is seconds/step [1] is PWM value array to be sent
     // values needed for dispense rate
-    var msecondsPerStep = PWMsteps[0] * 1000;
-    var PWMvalueArray = PWMsteps[1];
-    console.log(PWMvalueArray);
+    var msecondsPerStep = even_uL_steps_output.seconds_per_step * 1000; // must be in milliseconds
+    var PWMvalueArray = even_uL_steps_output.PWM_values;
+    var commandConversionTable = even_uL_steps_output.conversion_table; // conversions only for commands being sent at the moment (this way its easier to update the current volume)
+    console.log('commandConversionTable: ');
+    console.log(commandConversionTable);
     
     // set correct dispenser to command
     localStorage.dispenserToControl = dispenserID;
-    console.log("length: " + PWMvalueArray.length);
+    console.log("Command arrray: ");
+    console.log(PWMvalueArray);
     
     // iterate over command array at appropriate time intervals
     for(i = 0; i < PWMvalueArray.length; i++){
 
         (function () {
+            // need to re-define some variables here due to scope
             var iPrime = i;
             var dispenser_to_control = dispenserID;
-            var tempVolumeIncrease = volume.value/(time.value/(msecondsPerStep/1000));
             setTimeout(function(){
                 var temp = JSON.parse(localStorage.dispenserData);
-                if(dispOrientation === "push"){
-                    temp[dispenserID - 1]['Current_State'] = (parseFloat(temp[dispenserID - 1]['Current_State']) - parseFloat(time.value/(msecondsPerStep/1000))).toFixed(1).toString();
-                }
-                else {      // pull orientaion
-                    temp[dispenserID - 1]['Current_State'] = (parseFloat(temp[dispenserID - 1]['Current_State']) + parseFloat(time.value/(msecondsPerStep/1000))).toFixed(1).toString();
-                }
-                localStorage.dispenserData = JSON.stringify(temp);
-                sendCommandDispense(PWMvalueArray[iPrime]); 
-                updateDispenseProgressBar(dispenser_to_control);
+                temp[dispenserID - 1]['Current_State'] = (commandConversionTable[PWMvalueArray[iPrime]]).toString();
+                localStorage.dispenserData = JSON.stringify(temp);  // update local storage to correct new volume amount
+                sendCommandDispense(PWMvalueArray[iPrime]);     // now send command
+                updateDispenseProgressBar(dispenser_to_control);    // and update graphics
             }, i*msecondsPerStep);
         })();
-
-
-
-        // updateDispenseProgressBar(dispenserID);
-
-        // NEED TO SEND COMMAND WITH PWM VAL INSTEAD OF uL VALUE!!!!!!!!
-        // if (JSON.parse(localStorage.dispenserData)[dispenserID - 1]['Current_State'] < JSON.parse(localStorage.dispenserData)[dispenserID - 1]['Max']) {
-        //     // var temp = JSON.parse(localStorage.dispenserData);
-        //     // temp[dispenserID - 1]['Current_State'] = (temp[dispenserID - 1]['Current_State'] + 0.25);
-        //     // localStorage.dispenserData = JSON.stringify(temp);
-        //     console.log(PWMvalueArray[i]));
-        //     setInterval(sendCommandDispense(PWMvalueArray[i]), 6000);
-        //     // updateDispenseProgressBar(dispenserID);
-        // }
-        // else {
-        //     toastr.warning('You have already dispensed the full amount of this syringe.');
-        // }
     }
     return false;
 }

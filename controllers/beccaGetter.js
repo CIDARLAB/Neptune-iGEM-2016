341058a613666c674044f305b250e93a96388929
kestaskk <kestas@bu.edu>
2016-09-15 17:12:30 -0400

    Small changes

--- controllers/beccaGetter.js
+++ controllers/beccaGetter.js
@@ -7,11 +7,10 @@ const path = require('path');
 exports.getBecca = function(req, res)
 {
     var jsonPath = req.body.jsonpath;
+    console.log(jsonPath);
     var read = fs.createReadStream(jsonPath);
     var write = fs.createWriteStream('./public/uploads/Build_Verify/buildJSON.json');
     read.pipe(write,function(){
         res.end();
     });
-
-
-}
+};
